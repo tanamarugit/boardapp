f4c8a8252dcf16fedd7be971f943f3363e6a51ad
tanamaru <yt.0408.mmm@gmail.com>
2023-04-30 23:38:59 +0900

    createアクションまでコーディング

---
+++ app/controllers/boards_controller.rb
@@ -0,0 +1,24 @@
+class BoardsController < ApplicationController
+  def index
+    
+  end
+  
+  def new
+    @board = Board.new
+    
+  end
+  
+  def create
+    Board.create(board_params)
+    binding.pry
+  end
+  
+
+  private
+
+  def board_params
+    params.require(:board).permit(:name, :title, :body)
+  end
+  
+
+end
